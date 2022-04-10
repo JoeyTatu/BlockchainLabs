17af0a9b226e8526ed48799b7340a3d3dcf3d790
Joey Tatú <x15015556@Student.ncirl.ie>
2022-04-10 21:45:16 +0100

    Added crypto/wallet.js

---
+++ crypto/wallet.js
@@ -0,0 +1,4 @@
+// in our Metamask, we have a number of ethereum addressses
+// this code will show you how to create those addresses
+
+// using BIP39, BIP44
---
+++ test.js
@@ -0,0 +1,4 @@
+console.log("Hello World!");
+
+// https://studentncirl.sharepoint.com/sites/BlockchainConceptsPGDCLOUD_SEPMoodleTeam-21/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FBlockchainConceptsPGDCLOUD%5FSEPMoodleTeam%2D21%2FShared%20Documents%2FGeneral%2FRecordings%2FView%20Only%2FBlockchain%20Concepts%5FRemote%20Lab%2D20220219%5F140415%2DMeeting%20Recording%2Emp4&parent=%2Fsites%2FBlockchainConceptsPGDCLOUD%5FSEPMoodleTeam%2D21%2FShared%20Documents%2FGeneral%2FRecordings%2FView%20Only
+// 3:08:04
