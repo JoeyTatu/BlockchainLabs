a4f0e9e7170f02779d1a20e3d36d305ccf8f688d
Joey Tatú <x15015556@Student.ncirl.ie>
2022-04-11 00:46:18 +0100

    About to repro metamask

--- crypto/wallet.js
+++ crypto/wallet.js
@@ -2,6 +2,8 @@
 // this code will show you how to create those addresses
 
 // using BIP39, BIP44
+// BIP39 is the seed phrase.
+// BIP44 is the path.
 
 // To generate the ETH address:
 // An secp256K1 keypair
@@ -12,6 +14,9 @@
 // load the dependencies 
 const Wallet = require("ethereumjs-wallet").default;
 const keccak256 = require('keccak256');
+
+const bip39 = require("bip39");
+const { hdkey } = require("ethereumjs-wallet");
 
 const getWalletDetails = async() => {
     console.log("Async function started");
@@ -40,10 +45,27 @@ const getWalletDetails = async() => {
 
 const getHDWalletDetails = async() => {
     console.log("Making a HD wallet");
+    const mnemonic = "seed phrase seed phrase seed phrase seed phrase seed phrase seed phrase";
+
+    // let seed = bip39.mnemonicToSeed(mnemonic); //returns [object Promise]
+    let seed = await bip39.mnemonicToSeed(mnemonic); // "await" waits for correct seed before returning
+    // console.log(`Seed: ${seed}`);
+
+    let hdWallet = hdkey.fromMasterSeed(seed);
+    const path = "m/44'/60'/0'/0/0";
+    const path2 = "m/44'/60'/0'/0/1";
+    const wallet = await hdWallet.derivePath(path).getWallet();
+    console.log(`Wallet: ${wallet}`);
+    
+    const pubKey = wallet.getPublicKey();
+    const hashedPubKey = keccak256(pubKey).toString('hex');
+    console.log(`Hashed public key: 0x${hashedPubKey}`);
+    const ethAddress = hashedPubKey.substring(24);
+    console.log(`Eth address: 0x${ethAddress}`);
 
 }
 
 getHDWalletDetails();
 
 
-console.log("This is the wallet creation file");
+// console.log("This is the wallet creation file");
