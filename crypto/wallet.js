4f0c03dc3a19373447397d06fcfb945f5d411c9b
Joey Tatú <x15015556@Student.ncirl.ie>
2022-04-10 22:04:43 +0100

    Created async function

--- crypto/wallet.js
+++ crypto/wallet.js
@@ -2,3 +2,21 @@
 // this code will show you how to create those addresses
 
 // using BIP39, BIP44
+
+// To generate the ETH address:
+// An secp256K1 keypair
+// Then, we take the public key
+// keccak256 hash it 
+// drop the first 12 bytes
+
+// load the dependencies 
+const Wallet = require("ethereumjs-wallet").default;
+const keccak256 = require('keccak256');
+
+const getWalletDetails = async() => {
+    console.log("Async function started")
+}
+
+getWalletDetails();
+
+console.log("This is the wallet creation file");
