17ec130f29dba38f491ffd272954260b4749fbe1
Joey Tatú <x15015556@Student.ncirl.ie>
2022-04-11 19:44:52 +0100

    Addded for loop to loop through addresses

--- distribute.js
+++ distribute.js
@@ -1,9 +1,15 @@
 // Read in a file of account addresses
 // Count the number of addresses
 // Get the remaining supply between the accounts
+// Get 5% of remainng supply between the accounts
+// Loop through accounts running token transfer of 5%
 // Collect tea and medals
 
+let fs = require("fs");
+let contract = require("./contract.js");
+let BigNumber = require("big-number"); 
 require('dotenv').config();
+
 
 const infuraToken = process.env.INFURA_TOKEN;
 const contractAddress = process.env.CONTRACT_ADDRESS;
@@ -11,9 +17,6 @@ const ownerAddress = process.env.OWNER_ADDRESS;
 const privateKey = Buffer.from(process.env.PRIVATE_KEY, 'hex');
 
 const doDistro = async() => {
-    let fs = require("fs");
-    let contract = require("./contract.js");
-
     // Distribution addresses
     let disAddresses = fs.readFileSync("./accounts.txt", "utf8").split(",");
 
@@ -23,8 +26,17 @@ const doDistro = async() => {
     // console.log(`Token symbol: ${symbol}`)
 
     let reaminingSupply = await contract.getBalance(ownerAddress);
-    let fivePercent = reaminingSupply/20;
-    console.log(`5% of ${reaminingSupply} is ${fivePercent}`);
+    let ownerBal = new BigNumber(reaminingSupply);
+    let fivePercentOfBalance = ownerBal.div(20);
+    console.log(`Five % of ${ownerBal} is ${fivePercentOfBalance}`);
+
+    let noOfAddresses = disAddresses.length
+    let disAmount = fivePercentOfBalance.div(noOfAddresses);
+
+    for (i=0; i < noOfAddresses; i++){
+        console.log(`Distributing to ${disAddresses[i]} with ${disAmount} ${symbol}. Waiting for reply.`);
+        let returnValue = trandferToken(ownerAddress, disAddresses[i], disAmount);
+    }
 }
 
 doDistro();
