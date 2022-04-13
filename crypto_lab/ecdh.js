228cc7c4891e22dc4a4fc0d50653a3a3dbac2b55
Joey Tatú <x15015556@Student.ncirl.ie>
2022-04-13 16:31:59 +0100

    Added ecdh.js

---
+++ crypto_lab/ecdh.js
@@ -0,0 +1,12 @@
+// For ECDH
+// Alice has a private key and a public key (elliptic curve)
+// Bob has a private key and a public key
+
+// ALice gives Bob her public key
+// Bob gives Alice his public key
+
+// Alice and Bob will then agree on the same secret,
+// which they use to encrypt communications
+
+// This is the KEY EXCHANGE
+
